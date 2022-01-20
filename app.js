82f2caf9431a882cf9c2e056d35e6b63d9071a05
Sotos <st@DESKTOP-676BBER.localdomain>
2022-01-20 19:45:26 +0200

    third commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
- 
+const http = require("http")
+
+// const server = http.createServer((req, res) => {
+//     res.end("Welcome to our home page")
+//   })
+
+// Usimg Event Emitter API
+const server = http.createServer()
+// emits request event
+// subscribe to it/ listen for it/ respond to it
+server.on("request", (req, res) => {
+  res.end("Welcome")
+})
+
+server.listen(5000)
